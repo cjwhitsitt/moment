d097202adc841a2c5fb56360686267d840040607
Jay Whitsitt <jay.whitsitt@gmail.com>
2026-08-18 21:01:23 -0500

    Correct android build script to avoid multi-user installs

--- clients/mobile/install-android.sh
+++ clients/mobile/install-android.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 fvm flutter build apk --release
-adb devices | grep -v List | grep device | cut -f 1 | xargs -I {} -P 8 adb -s {} install -r build/app/outputs/flutter-apk/app-release.apk
+adb devices | grep -v List | grep device | cut -f 1 | xargs -I {} -P 8 adb -s {} install --user 0 -r build/app/outputs/flutter-apk/app-release.apk
